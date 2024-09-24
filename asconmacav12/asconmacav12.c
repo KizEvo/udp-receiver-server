fcdbe243e0ccf3afa55f7a11fc2f77c2ebeb4e27
KizEvo <vandungqc03@gmail.com>
2024-09-24 13:36:46 +0000

    asconmacav12: update calc mic, aes payload decrypt and display all info
    This commit marked the first step in supporting 1 sensor device package
    decryption.

--- asconmacav12/asconmacav12.c
+++ asconmacav12/asconmacav12.c
@@ -14,10 +14,13 @@
 /* x is pointer to unsigned char */
 #define LE_BYTES_TO_UINT32(x) ((*(x + 3)) << 24) | ((*(x + 2)) << 16) | ((*(x + 1)) << 8) | ((*(x)))
 #define LE_BYTES_TO_UINT16(x) ((*(x + 1)) << 8) | ((*(x)))
-
+#define LE_UINT32(x) (((x >> 24) & 0xFF) | ((x >> 8) & 0xFF00) | ((x << 8) & 0xFF0000) | ((x << 24) & 0xFF000000))
 static unsigned char nwskey1[CRYPTO_KEYBYTES] = {NWKSKEY1};
+static unsigned char appskey1[CRYPTO_KEYBYTES] = {APPSKEY1};
 
 static unsigned char tag[CRYPTO_BYTES] = { 0 };
+
+static uint32_t devices[] = {DEV_ADDR1};
 
 void reverse_bytes(uint8_t *bytes, size_t size);
 
@@ -59,11 +62,47 @@ int main(int argc, char *argv[])
         return -3;
     }
     uint32_t mic = 0;
-    loramac_calculate_mic(payload, frm_payload_size, nwskey1, 1, &mic);
-
-    printf("%x", mic);
+    uint32_t decoded_mic = 0;
+    /* We should loop for every keys here */
+    uint8_t curr_dev = 0;
+    for (; curr_dev < sizeof(devices) / sizeof(uint32_t); curr_dev++) {
+        loramac_calculate_mic(payload, frm_payload_size, nwskey1, 1, &mic);
+        decoded_mic = LE_BYTES_TO_UINT32(&decoded[LRMAC_BYTE_OFFSET_FRMPAYLOAD + frm_payload_size]);
+        if (mic == decoded_mic) {
+            curr_dev += 1;
+            break;
+        }
+    }
+    if (mic != decoded_mic) {
+        return -4;
+    }
+    /*
+     * Decrypt LoRaWAN payload
+     *
+     * Yes I know, the function name is 'encryption' then
+     * how can it decrypt ? The LoRaWAN payload encryption
+     * and decryption is special because the algorithm does
+     * not run with the payload as input but rather a block
+     * called A[16] which is specified in the spec. This get
+     * encrypted and produce S[16] and then it's XOR with the
+     * LoRaWAN payload.
+     *
+     * So now we can just run the encryption function. Which
+     * will produce the same S[16] and XOR with the encrypted
+     * data will produce the decrypted payload.
+     *
+     * Check the spec if this is not clear to you.
+     */
+    loramac_frm_payload_encryption(payload, frm_payload_size, appskey1);
+    for (uint8_t i = 0; i < frm_payload_size; i++) {
+        printf("%.2x", frm_payload[i]);
+    }
     printf("\n");
-
+    printf("%.2u\n", curr_dev);
+    printf("%x\n", dev_addr);
+    printf("%.4x\n", f_cnt);
+    printf("%.2x\n", f_port);
+    printf("%.2x\n", m_hdr);
     base64_cleanup();
     return 0;
 }
